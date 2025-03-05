ecf70e20ab7802eb6f84cb9af648aec266ed7a25
Adarsh Gupta <adarshguptajhs@gmail.com>
2025-03-05 15:45:27 +0530

    [Adarsh Gupta] add. sequence problems

---
+++ UnitConversion.js
@@ -0,0 +1,21 @@
+let inch = 12;
+let a = 42;
+let inchToFeet = a / inch;
+
+console.log("42 inch is equal to " + inchToFeet + " feet");
+
+
+let length = 60;
+let breadth = 40;
+let feetToMeter = 0.3048;
+let lengthInMeter = length * feetToMeter;
+let breadthInMeter = breadth * feetToMeter;
+
+console.log("Rectangular plot of 60 feet x 40 feet in meters is " + lengthInMeter + " x " + breadthInMeter + " meters");
+
+
+area = length * breadth;
+
+acre = area * 0.000247105;
+
+console.log("Area of 25 such plots in acres is " + acre * 25);
